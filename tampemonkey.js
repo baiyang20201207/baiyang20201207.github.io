53679ccbca28e8a33cac20bfe81f7bf42ccc65b5
baiyang2015 <1034242265@qq.com>
2020-12-08 15:15:23 +0800

    “1208-改”

---
+++ tampemonkey.js
@@ -0,0 +1,15 @@
+window.GM_setValue = function(){
+
+}
+window.GM_getValue = function(){
+
+}
+window.GM_addStyle = function(){
+
+}
+window.GM_registerMenuCommand = function(){
+
+}
+window.GM_xmlhttpRequest = function(obj){
+    $.ajax(obj)
+}
