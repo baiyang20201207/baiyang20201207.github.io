0d3be32c7f5299f9bd15b895c994e1bba03d1c24
baiyang20201207 <448625461@qq.com>
2021-01-05 10:18:28 +0800

    0105学习

---
+++ python学习/song/def/test/selenium_text.py
@@ -0,0 +1,12 @@
+#conding=utf-8
+#@time  - 
+#@author baiyang
+from selenium import webdriver
+from time import sleep
+driver = webdriver.Chrome()
+driver.get("http://www.testingedu.com.cn:8000/Home/user/login.html")
+driver.find_element_by_name('username').send_keys('13800138006')
+driver.find_element_by_name('password').send_keys('123456')
+driver.find_element_by_id('verify_code').send_keys('1')
+driver.find_element_by_name('sbtbutton').click()
+sleep(5)
